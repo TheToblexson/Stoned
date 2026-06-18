ba1799c52ca1290139dd7b2a15ef1e397e96b43a
Toblexson <71195995+TheToblexson@users.noreply.github.com>
2026-06-18 12:26:29 +0100

    Updated README

--- src/main/java/net/toblexson/stoned/Stoned.java
+++ src/main/java/net/toblexson/stoned/Stoned.java
@@ -20,7 +20,7 @@ public class Stoned
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "stoned";
     // Directly reference a slf4j logger
-    //private static final Logger LOGGER = LogUtils.getLogger();
+    // private static final Logger LOGGER = LogUtils.getLogger();
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
